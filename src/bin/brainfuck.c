9cea697b91ed56d76c3bd1a35100b41fb6e13718
JulMan <julman@julman.fr>
2026-05-15 19:25:00 +0200

    refactor: rewrite of bf bin using abstraction Now using bf_prepares_bf instead of direct bf_evals now deprecated.

--- src/bin/brainfuck.c
+++ src/bin/brainfuck.c
@@ -11,6 +11,7 @@ int main(int argc, const char **argv)
 
     int jit = 0;
     FILE *input = stdin, *output = stdout, *file = NULL;
+    const char *file_name = NULL;
 
     if (argc == 1)
     {
@@ -90,11 +91,12 @@ print_usage:
         }
 
         file = fopen(arg, "r");
+        file_name = arg;
 
         if (!file)
         {
             error = errno;
-            fprintf(stderr, "%s: %s: %s\n", argv[0], argv[i], strerror(error));
+            fprintf(stderr, "%s: %s: %s\n", argv[0], arg, strerror(error));
             goto close_files;
         }
     }
@@ -111,7 +113,7 @@ print_usage:
     char *buf = malloc(size + 1);
     if (fread(buf, 1, size, file) < size)
     {
-        fprintf(stderr, "Failed to read %s.\n", argv[1]);
+        fprintf(stderr, "%s: %s: %s.\n", argv[0], file_name, strerror(errno));
         fclose(file);
         free(buf);
         return 1;
@@ -132,18 +134,45 @@ print_usage:
     bf_set_output(state, output);
     setbuf(output, NULL);
 
+    bf_code_t code = NULL;
+
+    if ((error = bf_prepares_bf(state, buf, size, &code)))
+    {
+        if (!code)
+        {
+            // not enough memory, nothing to do
+            goto deinit;
+        }
+
+        const char *error_message;
+        size_t error_offset;
+
+        bf_code_error(state, code, &error_message, &error_offset);
+
+        fprintf(stderr, "syntax error: %s:%zu: %s\n", file_name, error_offset + 1, error_message);
+        goto free_code;
+    }
+
     if (jit)
     {
-        error = bf_evals_jit(state, buf, size);
+        error = bf_evalp_jit(state, code);
     }
     else
     {
-        error = bf_evals(state, buf, size);
+        error = bf_evalp(state, code);
     }
 
+    if (error)
+    {
+        fprintf(stderr, "runtime error: %s\n", strerror(error));
+    }
+
+free_code:
+    bf_code_free(state, code);
+
+deinit:
     bf_deinit(state);
 
-free_buf:
     free(buf);
 
 close_files:
